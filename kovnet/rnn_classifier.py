7fb96cd7d7100f06559e69848b43de975bec893b
kenkov <kenkovtan@gmail.com>
2018-06-05 01:23:47 +0900

    PadRNNを利用

--- kovnet/rnn_classifier.py
+++ kovnet/rnn_classifier.py
@@ -2,12 +2,14 @@
 # coding:utf-8
 
 
+import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from kovnet.vectorizer import IDVectorizer
 from kovnet.utils import sort_by_length
+from kovnet.rnn import PadRNN
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -17,21 +19,15 @@ class RNNClassifier(nn.Module):
         super(self.__class__, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim,
                                       padding_idx=0)
-        self.rnn = nn.GRU(embedding_dim, hidden_dim, 1,
-                          dropout=0)
+        self.rnn = PadRNN(embedding_dim, hidden_dim, 1,
+                          dropout=0, rnn_class=nn.GRU)
         self.out_lin = nn.Linear(hidden_dim, label_size)
 
     def forward(self, vec, lengths):
         emb = self.embedding(vec)
 
         # RNN forward
-        x = nn.utils.rnn.pack_padded_sequence(emb, lengths)
-        rnn_out_, rnn_hidden = self.rnn(x)
-        rnn_out, _ = nn.utils.rnn.pad_packed_sequence(rnn_out_)
-
-        rnn_out_final = rnn_out[[idx.item()-1 for idx in lengths],
-                                range(vec.shape[1])]
-
+        rnn_out, rnn_hidden, rnn_out_final = self.rnn(emb, lengths)
         out = self.out_lin(rnn_out_final)
 
         return out
@@ -65,7 +61,7 @@ if __name__ == "__main__":
     loss_function = nn.CrossEntropyLoss(ignore_index=0)
     optimizer = optim.Adam(model.parameters())
 
-    for epoch in range(1000):
+    for epoch in range(100):
         batch_perm = torch.randperm(num_samples)
         for batch_idx in range(0, num_samples, batch_size):
             # initialize grad
@@ -87,13 +83,16 @@ if __name__ == "__main__":
             # forward
             out = model.forward(X, lengths)
             loss = loss_function(out, y)
-            print("Epoch: {}, loss: {:.6f}".format(epoch, loss.item()))
-            print("> {}\n= {}\n< {}".format(
-                samples[-1],
-                targets[-1],
-                label_encoder.classes_[out[-1].topk(1)[1].item()],
-                ))
 
             # backward
             loss.backward()
             optimizer.step()
+
+            # log
+            print("Epoch: {}, loss: {:.6f}".format(epoch, loss.item()))
+            sample_idx = random.choice(range(X.shape[1]))
+            print("> {}\n= {}\n< {}".format(
+                samples[sample_idx],
+                targets[sample_idx],
+                label_encoder.classes_[out[sample_idx].topk(1)[1].item()],
+                ))
