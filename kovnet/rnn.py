b322ab3a8c51f6a75536e687a7b5b19d6fa763d4
kenkov <kenkovtan@gmail.com>
2018-06-06 22:38:20 +0900

    Decoder を実装

--- kovnet/rnn.py
+++ kovnet/rnn.py
@@ -20,3 +20,28 @@ class PadRNN(nn.Module):
                                 range(vec.shape[1])]
 
         return rnn_out, rnn_hidden, rnn_out_final
+
+
+class Decoder(nn.Module):
+    def __init__(self,
+                 num_embeddings,
+                 embedding_dim,
+                 hidden_size,
+                 num_layers=1,
+                 dropout=0):
+        super(self.__class__, self).__init__()
+
+        # hyperparameters
+        self.embedding = nn.Embedding(num_embeddings,
+                                      embedding_dim,
+                                      padding_idx=0)
+        self.rnn = PadRNN(embedding_dim, hidden_size, num_layers,
+                          dropout=dropout, rnn_class=nn.GRU)
+        self.out_lin = nn.Linear(hidden_size, num_embeddings)
+
+    def forward(self, vec, lengths):
+        emb = self.embedding(vec)
+        rnn_out, rnn_hidden, rnn_out_final = self.rnn(emb, lengths)
+        out = self.out_lin(rnn_out)
+
+        return out
